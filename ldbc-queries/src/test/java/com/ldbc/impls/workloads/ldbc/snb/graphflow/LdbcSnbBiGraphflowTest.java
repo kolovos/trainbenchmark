24a7669f28d1fb1848434b7a4c2ade20cac00116
Gabor Szarnyas <szarnyasg@gmail.com>
2017-11-08 15:37:46 -0500

    Removed unused class

--- ldbc-queries/src/test/java/com/ldbc/impls/workloads/ldbc/snb/graphflow/LdbcSnbBiGraphflowTest.java
+++ ldbc-queries/src/test/java/com/ldbc/impls/workloads/ldbc/snb/graphflow/LdbcSnbBiGraphflowTest.java
@@ -9,9 +9,6 @@ import java.util.Map;
 
 public class LdbcSnbBiGraphflowTest extends LdbcSnbBiQueryTest {
 
-	private final String endpoint = "bolt://localhost:7687";
-	private final String user = "neo4j";
-	private final String password = "admin";
 	private final String queryDir = "queries/";
 
 	public LdbcSnbBiGraphflowTest() throws DbException {
@@ -21,9 +18,6 @@ public class LdbcSnbBiGraphflowTest extends LdbcSnbBiQueryTest {
 	@Override
 	protected final Map<String, String> getProperties() {
 		Map<String, String> properties = new HashMap<>();
-		properties.put("endpoint", endpoint);
-		properties.put("user", user);
-		properties.put("password", password);
 		properties.put("queryDir", queryDir);
 		properties.put("printQueryNames", "true");
 		properties.put("printQueryStrings", "false");
